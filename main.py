3589e6403cb1fe897187e35e8cae2142ac887402
jakob-ra <jakob.rauch@student.uva.nl>
2022-04-04 14:22:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-pd.read_csv('https://www.dropbox.com/scl/fi/3eym9phxovppm6q1dwcqz/Besch-ftigungQuartalsdaten.xlsx?dl=1')
+df = pd.read_excel('https://www.dropbox.com/scl/fi/3eym9phxovppm6q1dwcqz/Besch-ftigungQuartalsdaten.xlsx?dl=1')
 
+print(df.head())
