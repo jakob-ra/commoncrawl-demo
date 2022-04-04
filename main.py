967a0b4c60468a53cff3fb6fc3638abcd306b13a
jakob-ra <jakob.rauch@student.uva.nl>
2022-04-04 14:24:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_excel('https://www.dropbox.com/scl/fi/3eym9phxovppm6q1dwcqz/Besch-ftigungQuartalsdaten.xlsx?dl=1')
+df = pd.read_csv('https://www.dropbox.com/s/p7n85yocpt59ahp/Country%20is%20CH%20AND%20Company%20type%20is%20Profit-new.csv?dl=1')
 
 print(df.head())
